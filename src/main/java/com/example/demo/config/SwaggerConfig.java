f9c654a7a6274ac5cc3824d693f7febcaf216c55
bala-amypo <bala@amypo.in>
2025-12-29 08:59:30 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -35,7 +35,6 @@ public class SwaggerConfig {
                         new Server().url("https://9093.32procr.amypo.ai/").description("Local Development"),
                         new Server().url("https://9093.32procr.amypo.ai/").description("Production")
                 ))
-                        new Server().url("").description("Local Development"),
 
                 // 🔐 Enable Authorize button
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
