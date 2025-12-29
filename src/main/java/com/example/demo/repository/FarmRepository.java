f753f5e5b6c5f222e7512ba0f4be816a508ae10a
bala-amypo <bala@amypo.in>
2025-12-29 06:44:31 +0000

    User commit

--- src/main/java/com/example/demo/repository/FarmRepository.java
+++ src/main/java/com/example/demo/repository/FarmRepository.java
@@ -2,12 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Farm;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
 public interface FarmRepository extends JpaRepository<Farm, Long> {
-    @Query("SELECT f FROM Farm f WHERE f.owner.id = ?1")
+
+    // Fetch all farms by owner user ID
     List<Farm> findByOwnerId(Long ownerId);
 }
